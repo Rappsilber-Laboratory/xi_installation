fa286195e15d47f9395f7c726418a5ab117369b5
Martin Graham <martinjamesgraham@outlook.com>
2018-04-10 13:49:29 +0100

    enable pgsql functionality in php.ini (off by default)

--- set_xi_phpinivalues.sh
+++ set_xi_phpinivalues.sh
@@ -1,4 +1,5 @@
-# This script sets some values in php.ini files needed for searchSubmit to upload large files
+#!/bin/sh
+# This script sets some values in php.ini files needed for searchSubmit to upload large files and to use a PGSQL database
 
 # find all instances of php.ini in this directory
 array=()
@@ -8,10 +9,9 @@ while IFS=  read -r -d $'\0'; do
 done <tmpfile
 rm -f tmpfile
 
-# and then replace the max sizes in those php.ini's with 4G
+# and then replace the max sizes in those php.ini's with 4G, and enable the pgsql functionality
 for i in "${array[@]}"
 do
   echo "$i"
-  sed -i.bak -e "s/^post_max_size=.*/post_max_size=4G/" -e "s/^upload_max_filesize=.*/upload_max_filesize=4G/" "$i"
+  sed -i.bak -e "s/^post_max_size=.*/post_max_size=4G/" -e "s/^upload_max_filesize=.*/upload_max_filesize=4G/" -e "s/^;extension=php_pdo_pgsql.dll/extension=php_pdo_pgsql.dll/" -e "s/^;extension=php_pgsql.dll/extension=php_pgsql.dll/" "$i"
 done
-
