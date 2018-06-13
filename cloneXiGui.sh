0f77ad2250ce091b835be30a6cbb5eac9c37e3d2
Martin Graham <martinjamesgraham@outlook.com>
2018-06-13 13:40:49 +0100

    update github clone script

--- cloneXiGui.sh
+++ cloneXiGui.sh
@@ -2,10 +2,11 @@ cd /var/www/html; # make this the web server directory you would like to use
 
 git clone https://github.com/Rappsilber-Laboratory/CLMS-model.git;
 git clone https://github.com/Rappsilber-Laboratory/searchSubmit.git;
-git clone https://github.com/Rappsilber-Laboratory/CLMS-UI.git;
+git clone https://github.com/Rappsilber-Laboratory/xiView.git;
 git clone https://github.com/Rappsilber-Laboratory/crosslink-viewer.git;
 git clone https://github.com/Rappsilber-Laboratory/spectrum.git;
 git clone https://github.com/Rappsilber-Laboratory/history.git;
 git clone https://github.com/Rappsilber-Laboratory/userGUI.git;
+git clone https://github.com/Rappsilber-Laboratory/vendor.git;
 
-mv CLMS-UI xi3; # this is necessary, other projects link to content in 'xi3'
+mv xiView xi3; # this is necessary, other projects link to content in 'xi3'
